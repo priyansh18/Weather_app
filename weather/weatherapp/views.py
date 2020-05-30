dda5dee605859ce4232496cddcd9dbe950b36f22
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-30 15:28:34 +0530

    Message,validation and error control

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -9,9 +9,31 @@ from .forms import cityForm
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=0daeff595e03682a91be65b4352751d8'
 
+    err_msg = ''
+    message = ''
+    message_class = ''
+
     if(request.method == 'POST'):
         form = cityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            exisisting_city_count = City.objects.filter(name=new_city).count()
+            if exisisting_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist'
+            else:
+                err_msg = "City Already in Database"
+
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added Sucessfully'
+            message_class = 'is-success'
 
     form = cityForm()
 
@@ -29,6 +51,7 @@ def index(request):
 
         weather_data.insert(0, city_weather)
 
-    context = {'weather_data': weather_data, 'form': form}
+    context = {'weather_data': weather_data, 'form': form,
+               'message': message, 'message_class': message_class}
 
     return render(request, 'weatherapp/weather.html', context)
